b96c7d3dadb8a49642753ae20657e89db32999c6
Syed Moazam Ali <107930498+MoazamAli45@users.noreply.github.com>
2025-05-25 00:44:07 +0500

    feat: Replace video background with GIF in HeroSection for improved compatibility

--- src/components/home/hero.jsx
+++ src/components/home/hero.jsx
@@ -60,7 +60,7 @@ const HeroSection = React.forwardRef(
         {...props}
         style={{ height: "calc(100vh)" }}
       >
-        <video
+        {/* <video
           className="absolute inset-0 w-full h-full object-cover z-0 opacity-30"
           autoPlay
           loop
@@ -69,7 +69,13 @@ const HeroSection = React.forwardRef(
         >
           <source src={"/assets/hero-new-bg.mp4"} type="video/mp4" />
           Your browser does not support the video tag.
-        </video>
+        </video> */}
+
+        <img
+          className="absolute inset-0 w-full h-full object-cover z-0 opacity-30"
+          src="https://media.giphy.com/media/ITRemFlr5tS39AzQUL/giphy.gif"
+          alt="Background GIF"
+        />
         <div className="absolute top-0 z-[0] h-full  w-screen bg-purple-950/10 bg-[radial-gradient(ellipse_20%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
         <section className="relative max-w-full h-full  mx-auto z-1">
           <RetroGrid {...gridOptions} />
